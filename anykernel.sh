e0f898db2e2d645e8c3011547143225bdf8f9100
Niranjan BR <niranjankannan2003@gmail.com>
2022-10-17 23:29:43 +0000

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Meraki Kernel for Redmi Note 10 Pro & Pro Max  by @mrfox2003
+kernel.string=Meraki Kernel by Niranjan
 do.devicecheck=1
 do.modules=0
 do.systemless=1
