a21653aa141263306be759ccb264f16f9ebcdeb4
kangeraarav <okand7946@gmail.com>
2022-12-04 09:09:19 +0000

    Sweet: Waybig kernel

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Meraki Kernel by Niranjan
+kernel.string=Waybig Kernel by Aarav
 do.devicecheck=1
 do.modules=0
 do.systemless=1
