076df6cb98589775c94cccfd155db133078d230c
Doctor Strange <faizanbashir380@gmail.com>
2023-06-03 20:20:30 +0530

    sweet: Welcome Dopaemon

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ElectroKernel by Aarav & Doctor Strange
+kernel.string=ElectroKernel by Aarav, Doctor Strange & KernelPanix
 do.devicecheck=1
 do.modules=0
 do.systemless=1
